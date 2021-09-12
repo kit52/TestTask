49970f388693a8eed7a13e0ba281a4ec0d3228a8
Aleksey <lesha.sinitsin@mail.ru>
2021-09-12 19:12:32 +0300

    fix bugs

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ import * as DB from "./DB.js"
 let currentPage = 0;
 let arr = [...DB.db];
 const color = [];
-
+let sortStateIndex = null;
+/* Create array unique color from get data */
 arr.forEach((item) => {
     color.push(item.eyeColor)
 })
-
 const uniqueColor = [...new Set(color)];
-
-
-
 const table = document.querySelector('.table');
 
-
+/* a function for rendering content in a table according to the page and saving sorting by the selected column */
 function renderTable(currentPage) {
     const tbody = document.createElement('tbody');
     let str = "";
@@ -22,31 +19,36 @@ function renderTable(currentPage) {
     /* Create dinamyc content for tbody  */
     table.querySelector('tbody').remove()
     for (let i = 0 + x; i <= 9 + x; i++) {
-        let row = `<tr>
-        <td> ${arr[i].name.firstName}</td> 
-        <td>${arr[i].name.lastName}</td>
-        <td class = "about ">${arr[i].about}</td>
-        <td class = ${arr[i].eyeColor}></td>
-    </tr>`
+        let row = `<tr key = ${arr[i].id}>
+            <td> ${arr[i].name.firstName}</td> 
+            <td>${arr[i].name.lastName}</td>
+            <td class = "about ">${arr[i].about}</td>
+            <td class = ${arr[i].eyeColor}></td>
+        </tr>`
         str = str + row;
     }
     tbody.innerHTML = str;
     table.append(tbody)
+
+    if (sortStateIndex != null) {
+        sortTable(sortStateIndex)
+    }
 }
 
-
+/*function for create dinamyc paginations */
 function renderPagination() {
-    /* Create dinamyc paginations */
     const paginationsDiv = document.createElement('div');
     const counterPages = arr.length / 10;
-
     let pages = "";
+
     for (let i = 1; i <= counterPages; i++) {
         let span = `<span ${i == currentPage + 1 ? 'class = "pageNumber active"' : "class = pageNumber"}>${i}</span>`
         pages = pages + span
     }
+
     paginationsDiv.innerHTML = pages;
     const paginations = document.querySelector(".paginations").appendChild(paginationsDiv);
+
     paginations.addEventListener("click", (e) => {
         if (e.target.classList == "pageNumber") {
             document.querySelectorAll(".pageNumber").forEach(item => item.classList.remove('active'))
@@ -57,11 +59,11 @@ function renderPagination() {
     })
 }
 
-
+/*Function for sorting table row A-Z */
 function sortTable(index) {
+    sortStateIndex = index;
     const tbody = document.querySelector('tbody');
     function compare(rowA, rowB) {
-
         const rowDataA = rowA.cells[index].innerHTML; /* get data from collumn[i]*/
         const rowDataB = rowB.cells[index].innerHTML;
         if (rowDataA == "" || rowDataB == "") {
@@ -75,7 +77,6 @@ function sortTable(index) {
             else if (rowDataA > rowDataB) return 1;
             return 0;
         }
-
     }
 
     const rows = [].slice.call(tbody.rows); /*converting a pseudo array to an array */
@@ -85,26 +86,37 @@ function sortTable(index) {
         tbody.appendChild(rows[i])
     }
     table.appendChild(tbody)
+
 }
+
+/* func for colouring selected item */
 function changeSelectColor(select) {
     const selectIndex = select.options.selectedIndex;
     select.style = `background:${select.options[selectIndex].classList.value}`
 }
 
-const changeTableContent = (e) => {
+function changeSelectedCollumnColor(i) {
+    document.querySelectorAll("th").forEach(item => item.classList.remove("bold"))
+    i.classList.add("bold")
+}
+
+/* func handler for all click by table */
+const tableClickHandler = (e) => {
     const container = document.querySelector(".container");
     const target = e.target;
-    console.log(target.nodeName);
     if (target.nodeName == "TH") {
+        changeSelectedCollumnColor(target)
         sortTable(target.cellIndex)
     } else if (target.nodeName == "IMG") {
+        changeSelectedCollumnColor(target.parentNode)
         sortTable(target.parentNode.cellIndex)
     } else {
-        table.removeEventListener("click", changeTableContent);
-        table.classList.add("modify")
-        const index = target.parentNode.rowIndex + currentPage * 10 - 1;
+        /* handler click by content row*/
+        table.removeEventListener("click", tableClickHandler);
+        table.classList.add("modify");
+        const id = target.parentNode.getAttribute("key");
+        let index = arr.findIndex(i => i.id == id);
         const element = arr[index];
-        const id = element.id;
 
         const optionColor = uniqueColor.map((item) => {
             return `<option style=background:${item}  ${item == element.eyeColor ? "selected" : ""} class = ${item}>
@@ -118,33 +130,28 @@ const changeTableContent = (e) => {
             <select id = select${id}>
                 ${optionColor}
             </select>
-            <textarea id = about${id} class = "aboutArea">
-                ${element.about}
-            </textarea>
+            <textarea id = about${id} class = "aboutArea">${element.about}</textarea>
 
         </form>
             <div class = "btn__save"></div>
         `
-        const divBody = document.createElement('div')
+        const divBody = document.createElement('div');
         divBody.innerHTML = form;
-        container.appendChild(divBody)
+        container.appendChild(divBody);
         const select = document.querySelector(`#select${id}`);
-        changeSelectColor(select)
+        changeSelectColor(select);
         select.addEventListener('change', () => changeSelectColor(select))
         const save = document.querySelector('.btn__save');
+
         save.addEventListener("click", (e) => {
             const form = divBody.querySelector('form');
             const firstNameValue = form.querySelector(`#firstName${id}`).value;
             const lastNameValue = form.querySelector(`#lastName${id}`).value;
             const aboutValue = form.querySelector(`#about${id}`).value;
-            const indexSelectItem = select.options.selectedIndex;
-            const selectItemValue = select.options[indexSelectItem].classList.value;
-            if (firstNameValue == element.name.firstName &&
-                lastNameValue == element.name.lastName &&
-                aboutValue == element.about &&
-                selectItemValue == element.eyeColor
-            ) return
-            else {
+            const selectItemValue = select.options[select.options.selectedIndex].classList.value;
+
+            if (firstNameValue != element.name.firstName || lastNameValue != element.name.lastName ||
+                aboutValue != element.about || selectItemValue != element.eyeColor) {
                 arr[index] = {
                     ...arr[index],
                     name: {
@@ -154,19 +161,18 @@ const changeTableContent = (e) => {
                     about: aboutValue,
                     eyeColor: selectItemValue
                 }
+                container.innerHTML = ''
+                renderTable(currentPage)
+            } else {
+                container.innerHTML = ''
             }
-            container.innerHTML = ''
-            renderTable(currentPage)
-            table.addEventListener("click", changeTableContent)
+
+            table.addEventListener("click", tableClickHandler)
             table.classList.remove("modify")
-
         })
-
     }
-
-
 }
-table.addEventListener('click', changeTableContent)
+table.addEventListener('click', tableClickHandler)
 
 renderTable(currentPage)
 renderPagination()
